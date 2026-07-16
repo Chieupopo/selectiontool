2716f72b5442b9381ce8e9aaf19e23599204ed3d
Chieupopo <chieupopo@users.noreply.github.com>
2026-07-16 23:41:06 +0700

    chore: cập nhật cấu hình Firebase mới và biến isFirebaseConfigured

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Cấu hình ứng dụng Firebase Web của bạn
 const firebaseConfig = {
-    apiKey: "AIzaSyCob6gT1WT6BCAZQWaRXYxaLhSInmst2eM",
-    authDomain: "inovance-selection-tool.firebaseapp.com",
-    projectId: "inovance-selection-tool",
-    storageBucket: "inovance-selection-tool.firebasestorage.app",
-    messagingSenderId: "233411610241",
-    appId: "1:233411610241:web:044e22d3c8903fb4f9af80",
-    measurementId: "G-YM32VR645Z"
+    apiKey: "AIzaSyDJ1wjR1a4dx9v7G54ehT6-URTkINYUjoE",
+    authDomain: "datanote-d38cc.firebaseapp.com",
+    projectId: "datanote-d38cc",
+    storageBucket: "datanote-d38cc.firebasestorage.app",
+    messagingSenderId: "858866785169",
+    appId: "1:858866785169:web:3eaad2a2a0ae9fe7ded830",
+    measurementId: "G-01XGEGTCNC"
 };
 
 // Đã cấu hình Firebase, kích hoạt đồng bộ đám mây
